6f26f85acf604e0acdc38c5c6e60f7129bb8ddfe
Deepak kumar <dk2984663@gmail.com>
2020-08-27 23:31:09 +0530

    recent changes

---
+++ anagram.py
@@ -0,0 +1,9 @@
+n=int(input())
+for i in range(n):
+    str1=list(input())
+    str2=list(input())
+    for j in str1[:]:
+        if j in str2:
+            str1.remove(j)
+            str2.remove(j)
+    print(len(str1)+len(str2))
